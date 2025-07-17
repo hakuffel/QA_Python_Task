212393c74ba5d930fae6489411bd0864be59ca74
Ekaterina@Hakuffel <katebutovo@mail.ru>
2025-07-17 18:26:14 +0300

    add new tests and markers

--- src/main.py
+++ src/main.py
@@ -2,10 +2,12 @@ import requests
 import json
 from src.typesettings import Gender
 
+
 def load_heroes():
-    response = requests.get('https://akabab.github.io/superhero-api/api/all.json')
+    response = requests.get("https://akabab.github.io/superhero-api/api/all.json")
     json_data = response.json()
     return json_data
+
 
 def filtered_gender_and_work(gender: Gender, check: bool):
     heroes = load_heroes()
@@ -16,31 +18,43 @@ def filtered_gender_and_work(gender: Gender, check: bool):
             f"Допустимые значения: {', '.join(valid_gender)}"
         )
     if not isinstance(check, bool):
-        raise ValueError(
-            "Параметр check должен быть True/False"
-        )
-    filtered_gender = [item for item in heroes if item['appearance']['gender'] == gender]
+        raise ValueError("Параметр check должен быть True/False")
+    filtered_gender = [
+        item for item in heroes if item["appearance"]["gender"] == gender
+    ]
     if check:
-        filter_both = [item for item in filtered_gender if item['work']['occupation'] != '-' or item['work']['base'] != '-']
+        filter_both = [
+            item
+            for item in filtered_gender
+            if item["work"]["occupation"] != "-" or item["work"]["base"] != "-"
+        ]
     else:
-        filter_both = [item for item in filtered_gender if item['work']['occupation'] == '-' and item['work']['base'] == '-']
+        filter_both = [
+            item
+            for item in filtered_gender
+            if item["work"]["occupation"] == "-" and item["work"]["base"] == "-"
+        ]
     return filter_both
 
 
 def get_height_cm(hero: dict):
-    height = hero['appearance'].get('height')
+    height = hero["appearance"].get("height")
     height_str = height[1]
-    if 'meter' in height_str:
+    if "meter" in height_str:
         return int(float(height_str.split()[0]) * 100)
     else:
         return int(height_str.split()[0])
 
+
 def tallest_hero_information(gender: Gender, check: bool):
     heroes = filtered_gender_and_work(gender, check)
     tallest_hero = max(heroes, key=get_height_cm)
-    tallest_hero = tallest_hero['id']
-    hero_information = requests.get(f'https://akabab.github.io/superhero-api/api/id/{tallest_hero}.json')
+    tallest_hero = tallest_hero["id"]
+    hero_information = requests.get(
+        f"https://akabab.github.io/superhero-api/api/id/{tallest_hero}.json"
+    )
     return hero_information.json()
+
 
 if __name__ == "__main__":
     print(json.dumps(tallest_hero_information("-", True), indent=4))
--- tests/test_functions.py
+++ tests/test_functions.py
@@ -1,6 +1,8 @@
 import pytest
 from src.main import filtered_gender_and_work, get_height_cm, tallest_hero_information
 
+
+@pytest.mark.positive
 def test_filter_female_with_work():
     result = filtered_gender_and_work("Female", True)
     assert len(result) > 0
@@ -8,6 +10,8 @@ def test_filter_female_with_work():
         assert hero["appearance"]["gender"] == "Female"
         assert hero["work"]["occupation"] != "-" or hero["work"]["base"] != "-"
 
+
+@pytest.mark.positive
 def test_filter_male_without_work():
     result = filtered_gender_and_work("Male", False)
     for hero in result:
@@ -15,6 +19,8 @@ def test_filter_male_without_work():
         assert hero["work"]["occupation"] == "-"
         assert hero["work"]["base"] == "-"
 
+
+@pytest.mark.positive
 def test_unknown_without_work():
     result = filtered_gender_and_work("-", False)
     for hero in result:
@@ -22,38 +28,64 @@ def test_unknown_without_work():
         assert hero["work"]["occupation"] == "-"
         assert hero["work"]["base"] == "-"
 
+
+@pytest.mark.positive
 def test_unknown_with_work():
     result = filtered_gender_and_work("-", True)
     for hero in result:
         assert hero["appearance"]["gender"] == "-"
         assert hero["work"]["occupation"] != "-" or hero["work"]["base"] != "-"
 
+
+@pytest.mark.negative
 def test_invalid_gender():
     with pytest.raises(ValueError):
         filtered_gender_and_work("Invalid", True)
 
+
+@pytest.mark.negative
 def test_invalid_check_work():
     with pytest.raises(ValueError):
         filtered_gender_and_work("Male", "pupupu")
 
 
+@pytest.mark.positive
 def test_get_height_cm():
     hero_cm = {"appearance": {"height": ["6'0", "180 cm"]}}
     assert get_height_cm(hero_cm) == 180
 
 
+@pytest.mark.positive
 def test_get_height_cm_if_meters():
     hero_meters = {"appearance": {"height": ["5'5", "1.65 meters"]}}
     assert get_height_cm(hero_meters) == 165
 
+
+@pytest.mark.positive
 def test_tallest_hero_female():
     result = tallest_hero_information("Female", True)
     assert result["appearance"]["gender"] == "Female"
 
+
+@pytest.mark.positive
 def test_tallest_hero_male():
     result = tallest_hero_information("Male", True)
     assert result["appearance"]["gender"] == "Male"
 
+
+@pytest.mark.positive
 def test_tallest_hero_unknown():
     result = tallest_hero_information("-", True)
     assert result["appearance"]["gender"] == "-"
+
+
+@pytest.mark.negative
+def test_tallest_hero_none_gender():
+    with pytest.raises(ValueError):
+        tallest_hero_information(None, True)
+
+
+@pytest.mark.negative
+def test_tallest_hero_none_work():
+    with pytest.raises(ValueError):
+        tallest_hero_information("Male", None)
